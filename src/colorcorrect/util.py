8505626e6ec4ff08840b0aa5166caaa40d27bcf8
aihara <s.aihara@gmail.com>
2014-12-29 23:07:03 +0900

    ver 0.05

--- src/colorcorrect/util.py
+++ src/colorcorrect/util.py
@@ -2,11 +2,13 @@
 import numpy as np
 import Image
 
+
 def from_pil(pimg):
     pimg = pimg.convert(mode='RGB')
     nimg = np.asarray(pimg)
     nimg.flags.writeable = True
     return nimg
+
 
 def to_pil(nimg):
     return Image.fromarray(np.uint8(nimg))
--- test/test_colorcorect.py
+++ test/test_colorcorect.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 import sys
 import Image
-from colorcorrect.algorithm import stretch, grey_world, retinex, max_white, retinex_adjust, standard_deviation_weighted_grey_world,standard_deviation_and_luminance_weighted_gray_world,automatic_color_equalization,luminance_weighted_gray_world,retinex_with_adjust
+from colorcorrect.algorithm import stretch, grey_world, retinex, retinex_with_adjust, max_white
+from colorcorrect.algorithm import standard_deviation_weighted_grey_world
+from colorcorrect.algorithm import standard_deviation_and_luminance_weighted_gray_world
+from colorcorrect.algorithm import automatic_color_equalization
+from colorcorrect.algorithm import luminance_weighted_gray_world
 from colorcorrect.util import from_pil, to_pil
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     img = Image.open(sys.argv[1])
     img.show()
     to_pil(stretch(from_pil(img))).show()
     to_pil(grey_world(from_pil(img))).show()
     to_pil(retinex(from_pil(img))).show()
     to_pil(max_white(from_pil(img))).show()
-    to_pil(retinex_adjust(retinex(from_pil(img)))).show()
-    to_pil(standard_deviation_weighted_grey_world(from_pil(img),20,20)).show()
-    to_pil(standard_deviation_and_luminance_weighted_gray_world(from_pil(img),20,20)).show()
-    to_pil(luminance_weighted_gray_world(from_pil(img),20,20)).show()
-    to_pil(automatic_color_equalization(from_pil(img),10,1000,500)).show()
+    to_pil(retinex_with_adjust(retinex(from_pil(img)))).show()
+    to_pil(standard_deviation_weighted_grey_world(from_pil(img), 20, 20)).show()
+    to_pil(standard_deviation_and_luminance_weighted_gray_world(from_pil(img), 20, 20)).show()
+    to_pil(luminance_weighted_gray_world(from_pil(img), 20, 20)).show()
+    to_pil(automatic_color_equalization(from_pil(img))).show()
